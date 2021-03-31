6d9707d5893b5ebcc523e874db5472c7b5dbbe9d
Aris Haryanto <arisharyanto32@gmail.com>
2021-04-01 00:33:39 +0700

    refactor for cqrs add elasticsearch

--- services/main.go
+++ services/main.go
@@ -6,15 +6,19 @@ import (
 	"net"
 
 	db "github.com/deposit-services/database"
-	deposit "github.com/deposit-services/proto"
+	event "github.com/deposit-services/proto"
+	"github.com/pkg/errors"
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
+
+	"github.com/deposit-services/nats"
 )
 
 type server struct {
-	command db.Command
-	query   db.Query
+	ev     db.EventStore
+	stream nats.Stream
+	query  db.Query
 }
 
 func main() {
@@ -25,7 +29,8 @@ func main() {
 	}
 
 	srv := grpc.NewServer()
-	deposit.RegisterAddServiceServer(srv, &server{})
+	event.RegisterEventStoreServer(srv, &server{})
+	event.RegisterAddServiceServer(srv, &server{})
 	reflection.Register(srv)
 
 	if e := srv.Serve(listener); e != nil {
@@ -33,31 +38,44 @@ func main() {
 	}
 }
 
-func (s *server) Deposit(ctx context.Context, request *deposit.DepositParam) (*deposit.Response, error) {
-	err := s.command.CreateDeposit(request.GetAmount(), request.GetFrom())
-	if err != nil {
-		return &deposit.Response{Status: int32(500), Message: string("Error create deposit")}, err
-	}
-
-	return &deposit.Response{Status: int32(200), Message: string("Success create deposit")}, nil
+//we need to define whenever we not use this
+func (s *server) Approve(ctx context.Context, request *event.ApproveParam) (*event.Response, error) {
+	return &event.Response{Status: int32(200), Message: string("Approve"), Data: []*event.Deposit{}}, nil
 }
 
-func (s *server) Approve(ctx context.Context, request *deposit.ApproveParam) (*deposit.Response, error) {
-	err := s.command.ApproveDeposit(request.GetIdDeposit())
-	if err != nil {
-		return &deposit.Response{Status: int32(500), Message: string("Error approve deposit")}, err
-	}
-
-	return &deposit.Response{Status: int32(200), Message: string("Success approve deposit")}, nil
+//we need to define whenever we not use this
+func (s *server) Deposit(ctx context.Context, request *event.DepositParam) (*event.Response, error) {
+	return &event.Response{Status: int32(200), Message: string("Deposit"), Data: []*event.Deposit{}}, nil
 }
 
-func (s *server) ListDeposit(ctx context.Context, request *deposit.ListDepositParam) (*deposit.Response, error) {
+func (s *server) ListDeposit(ctx context.Context, request *event.ListDepositParam) (*event.Response, error) {
 	listDeposit := s.query.GetDeposit()
 
 	fmt.Println(listDeposit)
 	if listDeposit == nil {
-		return &deposit.Response{Status: int32(500), Message: string("Error approve deposit"), Data: []*deposit.Deposit{}}, nil
+		return &event.Response{Status: int32(500), Message: string("Error approve deposit"), Data: []*event.Deposit{}}, nil
 	}
 
-	return &deposit.Response{Status: int32(200), Message: string("Get All List"), Data: listDeposit}, nil
+	return &event.Response{Status: int32(200), Message: string("Get All List"), Data: listDeposit}, nil
+}
+
+func (s *server) GetEvents(ctx context.Context, eventData *event.EventFilter) (*event.EventResponse, error) {
+
+	return &event.EventResponse{}, nil
+}
+
+func (s *server) CreateEvent(ctx context.Context, eventData *event.EventParam) (*event.ResponseParam, error) {
+	//insert kedalam event store dalam hal ini kita create ke table eventstore mysql
+	//  dengan data yang dikirim dari client eventData
+	createEvent := s.ev.CreateEvent(eventData)
+
+	//publish event melalui nats streaming dengan data yang dikirim dari client eventData
+	go s.stream.Publish(eventData.Channel, eventData)
+
+	fmt.Println(createEvent)
+	if createEvent == nil {
+		return &event.ResponseParam{}, errors.Wrap(createEvent, "error from RPC server")
+	}
+
+	return &event.ResponseParam{}, nil
 }
